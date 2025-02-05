184ffd4b26ebf547074b859b2cf5f9a3d3dbc42c
lloydie <lloydie.glophics@gmail.com>
2025-02-05 23:03:56 +0800

    Updates

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <v-container>
+            <v-row>
+                <v-col cols="12">
+                    <h1>Home Page</h1>
+                </v-col>
+            </v-row>
+
+        </v-container>
+    </div>
+</template>
