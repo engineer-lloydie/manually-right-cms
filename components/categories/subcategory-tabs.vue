6b953220319c77d28d52f1c23536203319dd7b37
lloydie <lloydie.glophics@gmail.com>
2025-02-11 23:47:34 +0800

    Updates

--- components/categories/subcategory-tabs.vue
+++ components/categories/subcategory-tabs.vue
@@ -4,7 +4,7 @@
             <v-tabs-window-item value="main">
                 <v-btn
                     color="red-lighten-1"
-                    @click="itemDialog = true"
+                    @click="itemDialog = true; loadMainCategories()"
                 >
                     Add New
                 </v-btn>
@@ -15,17 +15,22 @@
                     :items-length="totalItems"
                     :loading="loading"
                     item-value="name"
-                    @update:options="loadItems"
+                    @update:options="loadCategories"
                 >
                     <template #item.actions="{ item }">
                         <v-icon
                             class="me-2"
                             size="small"
-                            @click="itemDialog = true"
+                            @click="setItemToEdit(item); loadMainCategories()"
                         >
                             mdi-pencil
                         </v-icon>
-                        <v-icon size="small" @click="deleteConfirmation = true"> mdi-delete </v-icon>
+                        <v-icon 
+                            size="small" 
+                            @click="deleteConfirmation = true; selectedCategoryId = item.id"
+                        >
+                            mdi-delete
+                        </v-icon>
                     </template>
                 </v-data-table-server>
             </v-tabs-window-item>
@@ -33,39 +38,39 @@
 
         <!-- Dialogs -->
 		<v-dialog v-model="itemDialog" max-width="500">
-			<v-card title="Add subcategory">
+			<v-card :title="(editDialog ? 'Edit' : 'Add') + ' main category'">
 				<template v-slot:actions>
 					<v-btn
 						class="ml-auto"
 						text="Close"
-						@click="itemDialog = false"
+						@click="resetValues"
 					></v-btn>
 				</template>
 				<v-container>
-					<form @submit.prevent="submit">
-                        <v-select
-							v-model="mainCategory.value.value"
-							:error-messages="mainCategory.errorMessage.value"
+					<form @submit.prevent="handleFormSubmission">
+						<v-select
+							v-model="mainCategoryId.value.value"
+							:error-messages="mainCategoryId.errorMessage.value"
 							:items="mainCategoryLists"
+                            item-value="id"
+                            item-title="name"
 							label="Main Category"
 							variant="outlined"
+                            :loading="loadingMainCategories"
 						></v-select>
-						<v-text-field
+                        <v-text-field
 							v-model="name.value.value"
-							:counter="10"
 							:error-messages="name.errorMessage.value"
 							label="Name"
 							variant="outlined"
 						></v-text-field>
 						<v-text-field
 							v-model="description.value.value"
-							:counter="10"
 							label="Description (Optional)"
 							variant="outlined"
 						></v-text-field>
 						<v-text-field
 							v-model="urlSlug.value.value"
-							:counter="10"
 							:error-messages="urlSlug.errorMessage.value"
 							label="URL Slug"
 							variant="outlined"
@@ -85,6 +90,7 @@
 				</v-container>
 			</v-card>
 		</v-dialog>
+
 		<v-dialog v-model="deleteConfirmation" width="auto">
 			<v-card
 				max-width="400"
@@ -97,7 +103,25 @@
 						class="ms-auto"
 						text="Yes"
                         color="error"
-						@click="deleteConfirmation = false"
+						@click="deleteCategory"
+					></v-btn>
+				</template>
+			</v-card>
+		</v-dialog>
+
+        <v-dialog v-model="successDialog" width="auto">
+			<v-card
+				max-width="400"
+				prepend-icon="mdi-check"
+				:text="successMessage"
+				title="Success"
+			>
+				<template v-slot:actions>
+					<v-btn
+						class="ms-auto"
+						text="Close"
+                        color="grey"
+						@click="successDialog = false; successMessage = null"
 					></v-btn>
 				</template>
 			</v-card>
@@ -107,153 +131,33 @@
 
 <script setup>
 import { useField, useForm } from "vee-validate";
+const tab = ref("main");
 import * as yup from "yup";
 
-const tab = ref("main");
-
-const subCategories = [
-    {
-        main_category: "Dessert",
-        name: "Frozen Yogurt",
-        description: 159,
-        url_slug: 6.0,
-        status: 24,
-    },
-    {
-        main_category: "Candy",
-        name: "Jelly bean",
-        description: 375,
-        url_slug: 0.0,
-        status: 94,
-    },
-    {
-        main_category: "Chocolate",
-        name: "KitKat",
-        description: 518,
-        url_slug: 26.0,
-        status: 65,
-    },
-    {
-        main_category: "Pastry",
-        name: "Eclair",
-        description: 262,
-        url_slug: 16.0,
-        status: 23,
-    },
-    {
-        main_category: "Cookie",
-        name: "Gingerbread",
-        description: 356,
-        url_slug: 16.0,
-        status: 49,
-    },
-    {
-        main_category: "Dessert",
-        name: "Ice cream sandwich",
-        description: 237,
-        url_slug: 9.0,
-        status: 37,
-    },
-    {
-        main_category: "Candy",
-        name: "Lollipop",
-        description: 392,
-        url_slug: 0.2,
-        status: 98,
-    },
-    {
-        main_category: "Pastry",
-        name: "Cupcake",
-        description: 305,
-        url_slug: 3.7,
-        status: 67,
-    },
-    {
-        main_category: "Candy",
-        name: "Honeycomb",
-        description: 408,
-        url_slug: 3.2,
-        status: 87,
-    },
-    {
-        main_category: "Pastry",
-        name: "Donut",
-        description: 452,
-        url_slug: 25.0,
-        status: 51,
-    },
-];
-
-const FakeAPI = {
-	async fetch({ page, itemsPerPage, sortBy }) {
-		return new Promise((resolve) => {
-			setTimeout(() => {
-				const start = (page - 1) * itemsPerPage;
-				const end = start + itemsPerPage;
-				const items = subCategories.slice();
-
-				if (sortBy.length) {
-					const sortKey = sortBy[0].key;
-					const sortOrder = sortBy[0].order;
-					items.sort((a, b) => {
-						const aValue = a[sortKey];
-						const bValue = b[sortKey];
-						return sortOrder === "desc"
-							? bValue - aValue
-							: aValue - bValue;
-					});
-				}
-
-				const paginated = items.slice(start, end);
-
-				resolve({ items: paginated, total: items.length });
-			}, 500);
-		});
-	},
-};
-
 const headers = ref([
-    {
-		title: "Main Category",
-		align: "start",
-        sortable: false,
-		key: "main_category",
-	},
-	{
-		title: "Name",
-		align: "end",
-		sortable: false,
-		key: "name",
-	},
-	{ title: "Description", key: "description", align: "end" },
-	{ title: "URL Slug", key: "url_slug", align: "end" },
-	{ title: "Status", key: "status", align: "end" },
+    { title: "Main Categories", key: "main_category", align: "start" },
+	{ title: "Name", key: "name", align: "start" },
+	{ title: "URL Slug", key: "url_slug", align: "start", sortable: false },
+	{ title: "Status", key: "status", align: "start", sortable: false },
 	{ title: "Actions", key: "actions", align: "end" },
 ]);
 
 const serverItems = ref([]);
 const loading = ref(true);
 const totalItems = ref(0);
-
-const itemsPerPage = ref(5);
-
-const loadItems = async ({ page, itemsPerPage, sortBy }) => {
-	loading.value = true;
-	const { items, total } = await FakeAPI.fetch({
-		page,
-		itemsPerPage,
-		sortBy,
-	});
-	serverItems.value = items;
-	totalItems.value = total;
-	loading.value = false;
-};
-
+const itemsPerPage = ref(10);
 const itemDialog = ref(false);
+const editDialog = ref(false);
 const deleteConfirmation = ref(false);
+const successDialog = ref(false);
+const successMessage = ref(null);
+const selectedCategoryId = ref(null);
+
+const loadingMainCategories = ref(false);
+const mainCategoryLists = ref([]);
 
 const schema = yup.object({
-	mainCategory: yup.string().required("Main category is required."),
+    mainCategoryId: yup.number().required("Main category is required."),
     name: yup.string().required("Name is required."),
     urlSlug: yup.string().required("URL slug is required."),
 	status: yup.string().required("Status is required."),
@@ -266,18 +170,126 @@ const { handleSubmit, handleReset } = useForm({
     },
 });
 
-const mainCategory = useField("mainCategory");
+const mainCategoryId = useField("mainCategoryId");
 const name = useField("name");
 const description = useField("description");
 const urlSlug = useField("urlSlug");
 const status = useField("status");
-
 const statusItems = ref(["Active", "Inactive"]);
-const mainCategoryLists = ref(["Main Category 1", "Main Category 2"]);
 
-const submit = handleSubmit((values) => {
-	alert(JSON.stringify(values, null, 2));
+const loadCategories = async ({ page, itemsPerPage, sortBy }) => {
+    try {
+        loading.value = true;
+
+        const { data, total } = await useBaseFetch('/sub-categories', {
+            method: 'GET',
+            params: {
+                page,
+                itemsPerPage,
+                sortBy,
+            }
+        });
+
+        serverItems.value = data;
+        totalItems.value = total;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loading.value = false;
+    }
+};
+
+const loadMainCategories = async () => {
+    try {
+        loadingMainCategories.value = true;
+
+        const data = await useBaseFetch('/main-categories', {
+            method: 'GET'
+        });
+
+        mainCategoryLists.value = data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loadingMainCategories.value = false;
+    }
+};
+
+const resetValues = () => {
+    itemDialog.value = false;
+    editDialog.value = false;
+    handleReset()
+}
+
+const handleFormSubmission = handleSubmit(async (values) => {
+    if (!editDialog.value) {
+        addCategory(values);
+    } else {
+        updateCategory(values);
+    }
 });
+
+const addCategory = async (values) => {
+	try {
+        const { message } = await useBaseFetch('/sub-categories', {
+            method: 'POST',
+            body: values
+        });
+
+        itemDialog.value = false
+        successMessage.value = message;
+        loadCategories({page: 1, itemsPerPage: 10});
+        successDialog.value = true;
+        handleReset();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const setItemToEdit = (item) => {
+    editDialog.value = true;
+    itemDialog.value = true;
+
+    selectedCategoryId.value = item.id
+    mainCategoryId.value.value = mainCategoryLists.value.find(list => list.main_category_id == item.main_category_id).id;
+    name.value.value = item.name;
+    description.value.value = item.description;
+    urlSlug.value.value = item.url_slug;
+    status.value.value = item.status;
+}
+
+const updateCategory = async (values) => {
+	try {
+        const { message } = await useBaseFetch(`/sub-categories/${selectedCategoryId.value}`, {
+            method: 'PUT',
+            body: values
+        });
+
+        itemDialog.value = false;
+        editDialog.value = false;
+        successMessage.value = message;
+        loadCategories({page: 1, itemsPerPage: 10});
+        successDialog.value = true;
+        handleReset();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const deleteCategory = async () => {
+	try {
+        const { message } = await useBaseFetch(`/sub-categories/${selectedCategoryId.value}`, {
+            method: 'DELETE'
+        });
+
+        deleteConfirmation.value = false
+        successMessage.value = message;
+        loadCategories({page: 1, itemsPerPage: 10});
+        successDialog.value = true;
+    } catch (error) {
+        console.error(error);
+    }
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ store/main-categories.mjs
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useMainCategoryStore = defineStore("mainCategories", {
+    state: () => {
+        return {
+           
+        };
+    },
+    actions: {
+        
+    }
+});
