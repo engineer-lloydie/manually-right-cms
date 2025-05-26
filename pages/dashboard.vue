7190e557761fdf4ea47470a855d1553660524533
lloydie <adolfojohnlloyd5@gmail.com>
2025-05-26 17:40:03 +0800

    Dashboard Partial Updates

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>
-        Home Page
-    </div>
+	<div>
+		<h3>Dashboard</h3>
+		<v-divider class="my-5"></v-divider>
+		<v-card>
+			<v-row>
+                <v-cols>
+                    <v-sheet>
+                        
+                    </v-sheet>
+                </v-cols>
+            </v-row>
+		</v-card>
+	</div>
 </template>
 
 <script setup>
+
 definePageMeta({
     title: 'Dashboard'
 });
+
 </script>
+
+<style lang="scss" scoped>
+</style>
